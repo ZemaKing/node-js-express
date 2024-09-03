baff3e22c96a3fdb6908533f36dc6aced3e5e31d
Milos Radulovic <milos.radulovic@hotmail.rs>
2024-09-03 08:43:59 +0200

    70. Creating HTML Pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.send('<h1>Hello from Express!</h1>');
+	// __dirname to file shop.js, then back, and into views
+	res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
